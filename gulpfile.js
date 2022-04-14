28f07824ec657c594bbde6bca58b6b13e3096187
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-04-14 10:29:43 +0430

    edit

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const {src , dest , watch , series} = require("gulp")
-const sass = require("gulp-sass")(require("sass"))
-
-function buildStyles (){
-    return src("index.scss")
-    .pipe(sass())
-    .pipe(dest("css"))
-}
